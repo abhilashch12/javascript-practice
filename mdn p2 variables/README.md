c1bd10fd750cce66b0410a40e8b41783f378acbd
abhilahch12 <abhilashreddych12@gmail.com>
2025-05-01 12:08:46 +0530

    mdn program about variables

---
+++ mdn p2 variables/README.md
@@ -0,0 +1,5 @@
+all about variable 
+1.decalartion
+2.intialization
+3.updating
+types of variables 1. whole numbers 2. string 3. boolean 4. array 5. object
---
+++ mdn p2 variables/script.js
@@ -0,0 +1,10 @@
+let myAge=25;//interger 
+myAge=24;
+console.log(myAge);
+let myName= "Abhilash";//string
+let voterId = myAge>=18;// boolean
+let colorsNames = ["red","green","yellow"];//Array
+console.log(colorsNames[0]);
+let car = {name:"bmw", color:"red"};
+console.log(car.name);
+
