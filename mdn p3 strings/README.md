34cca61cb7e21f3d2d126324ac12f4ebeecf7680
abhilahch12 <abhilashreddych12@gmail.com>
2025-05-02 10:30:45 +0530

    strings

---
+++ mdn p3 strings/README.md
@@ -0,0 +1,3 @@
+*string declaration
+*converting number to string and string to number
+*using template literals for manipulation
---
+++ mdn p3 strings/script.js
@@ -0,0 +1,17 @@
+let para = "have a nice day";
+console.log(para);
+let userName = "ab, ";
+let greeting = "have a good day";
+let result = `${userName}${greeting}`;
+console.log(result);
+//writing in multiple lines using template literals
+let paragraph = `hi, how r u
+ nice to see u`;
+ console.log(paragraph);
+ //converting string to number and viceversa
+let score="25";
+let numeric = Number(score);
+console.log(typeof(numeric));
+let num1= 30;
+let strg= String(num1);
+console.log(typeof(strg));
