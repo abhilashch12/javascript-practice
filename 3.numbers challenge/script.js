48affa4125da04b2423555a80d060f4cf5731668
abhilahch12 <abhilashreddych12@gmail.com>
2025-05-03 14:19:13 +0530

    Number challenge

---
+++ 3.numbers challenge/script.js
@@ -0,0 +1,13 @@
+const score1=20;
+const score2=23;
+const score3=44;
+const totalScore = score1+score2+score3;
+const averageScore = totalScore/3;
+console.log(`totalscore is :${totalScore} and averagescore is : ${averageScore}`);
+const plates=20;
+const people=7;
+let remainingPlates = plates-people;
+remainingPlates += 1;
+const message = `There are ${ remainingPlates} plates available`;
+console.log(message);
+
