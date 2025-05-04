79dcb07d7381c7fe3a66e94ec85fa7bf4d387bba
abhilahch12 <abhilashreddych12@gmail.com>
2025-05-04 10:02:17 +0530

    full name exercise

---
+++ 9.FULL NAME EXERCISE(ARRAYS AND LOOP)/script.js
@@ -0,0 +1,8 @@
+const names = ["anna","susy","bob"];
+const lastName = "shakeandbake";
+let newArray =[];
+for(i=0;i<3;i++){
+    newArray[i]= `${names[i]} ${lastName}`;
+    console.log(newArray[i]);
+}
+//console.log(newArray[]);
